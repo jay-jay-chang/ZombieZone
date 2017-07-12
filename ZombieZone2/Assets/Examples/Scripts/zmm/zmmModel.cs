224a731e91836c9118aca64f77db12762020812f
jjay <greenoath@gmail.com>
2017-07-12 22:47:27 +0800

    new art apply

---
+++ ZombieZone2/Assets/Examples/Scripts/zmm/zmmModel.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Spine.Unity.Examples {
+	[SelectionBase]
+	public class zmmModel : MonoBehaviour {
+
+		#region Inspector
+		[Header("Current State")]
+		public zmmBodyState state;
+		public bool facingLeft;
+		[Range(-1f, 1f)]
+		public float currentSpeed;
+
+		[Header("Balance")]
+		public float shootInterval = 0.12f;
+		#endregion
+
+		float lastShootTime;
+		public event System.Action ShootEvent;	// Lets other scripts know when Spineboy is shooting. Check C# Documentation to learn more about events and delegates.
+
+		#region API
+
+		public void TryShoot () {
+			float currentTime = Time.time;
+
+			if (currentTime - lastShootTime > shootInterval) {
+				lastShootTime = currentTime;
+				if (ShootEvent != null) ShootEvent();	// Fire the "ShootEvent" event.
+			}
+		}
+
+		public void TryMove (float speed) {
+			currentSpeed = speed; // show the "speed" in the Inspector.
+
+			if (speed != 0) {
+				bool speedIsNegative = (speed < 0f);
+				facingLeft = speedIsNegative; // Change facing direction whenever speed is not 0.
+			}
+				
+			state = (speed == 0) ? zmmBodyState.Idle : zmmBodyState.Running;
+
+		}
+		#endregion
+	}
+
+	public enum zmmBodyState {
+		Idle,
+		Running
+	}
+}
---
+++ ZombieZone2/Assets/Examples/Scripts/zmm/zmmView.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+
+namespace Spine.Unity.Examples {
+	public class zmmView : MonoBehaviour {
+
+		#region Inspector
+		[Header("Components")]
+		public zmmModel model;
+		public SkeletonAnimation skeletonAnimation;
+
+		[SpineAnimation] public string run, idle, fire;
+//		[SpineEvent] public string footstepEventName;
+
+//		[Header("Audio")]
+//		public float footstepPitchOffset = 0.2f;
+//		public float gunsoundPitchOffset = 0.13f;
+//		public AudioSource footstepSource, gunSource, jumpSource;
+
+//		[Header("Effects")]
+//		public ParticleSystem gunParticles;
+		#endregion
+
+		zmmBodyState previousViewState;
+
+		void Start () {
+			if (skeletonAnimation == null) return;
+			model.ShootEvent += PlayShoot;
+//			skeletonAnimation.AnimationState.Event += HandleEvent;
+		}
+
+//		void HandleEvent (Spine.TrackEntry trackEntry, Spine.Event e) {
+//			if (e.Data.Name == footstepEventName)
+//				PlayFootstepSound();
+//		}
+
+		void Update () {
+			if (skeletonAnimation == null) return;
+			if (model == null) return;
+
+			if (skeletonAnimation.skeleton.FlipX != model.facingLeft) {	// Detect changes in model.facingLeft
+				Turn(model.facingLeft);
+			}
+
+			// Detect changes in model.state
+			var currentModelState = model.state;
+
+			if (previousViewState != currentModelState) {
+				PlayNewStableAnimation();
+			}
+
+			previousViewState = currentModelState;
+		}
+
+		void PlayNewStableAnimation () {
+			var newModelState = model.state;
+			string nextAnimation;
+
+			// Add conditionals to not interrupt transient animations.
+
+//			if (previousViewState == SpineBeginnerBodyState.Jumping && newModelState != SpineBeginnerBodyState.Jumping) {
+//				PlayFootstepSound();
+//			}
+
+//			if (newModelState == SpineBeginnerBodyState.Jumping) {
+//				jumpSource.Play();
+//				nextAnimation = jump;
+//			} else {
+				if (newModelState == zmmBodyState.Running) {
+					nextAnimation = run;
+				} else {
+					nextAnimation = idle;
+				}
+//			}
+
+			skeletonAnimation.AnimationState.SetAnimation(0, nextAnimation, true);
+		}
+
+//		void PlayFootstepSound () {
+//			footstepSource.Play();
+//			footstepSource.pitch = GetRandomPitch(footstepPitchOffset);
+//		}
+
+		[ContextMenu("Check Tracks")]
+		void CheckTracks () {
+			var state = skeletonAnimation.AnimationState;
+			Debug.Log(state.GetCurrent(0));
+			Debug.Log(state.GetCurrent(1));
+		}
+
+		#region Transient Actions
+		public void PlayShoot () {
+			// Play the shoot animation on track 1.
+			var track = skeletonAnimation.AnimationState.SetAnimation(1, fire, false);
+			track.AttachmentThreshold = 1f;
+			track.MixDuration = 0f;
+			var empty = skeletonAnimation.state.AddEmptyAnimation(1, 0.5f, 0.1f);
+			empty.AttachmentThreshold = 1f;
+//			gunSource.pitch = GetRandomPitch(gunsoundPitchOffset);
+//			gunSource.Play();
+//			//gunParticles.randomSeed = (uint)Random.Range(0, 100);
+//			gunParticles.Play();
+		}
+
+		public void Turn (bool facingLeft) {
+			skeletonAnimation.Skeleton.FlipX = facingLeft;
+			// Maybe play a transient turning animation too, then call ChangeStableAnimation.
+		}
+		#endregion
+
+		#region Utility
+		public float GetRandomPitch (float maxPitchOffset) {
+			return 1f + Random.Range(-maxPitchOffset, maxPitchOffset);
+		}
+		#endregion
+	}
+
+}
