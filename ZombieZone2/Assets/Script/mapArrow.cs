e27c23659510d472fe22f78d312fae8571db0935
jay <jay@maicity.com>
2017-08-16 12:17:04 +0800

    add flow shader!!!

--- ZombieZone2/Assets/Script/mapArrow.cs
+++ ZombieZone2/Assets/Script/mapArrow.cs
@@ -14,14 +14,22 @@ public class mapArrow : MonoBehaviour {
 	public Vector3 start;
 	public Vector3 end;
 
+	public SpriteRenderer sRenderer;
+	public float speed;
+
 	List < GameObject > createdObject;
+	float updateTime;
 
 	void Start () {
+		sRenderer.material.SetFloat ("_Speed", speed);
 	}
 
 	void OnModify(){
 		Vector3 dir = end - start;
 		makeLine (dir.magnitude);
+
+		this.gameObject.transform.localPosition = new Vector3 (0.5f*(end.x + start.x), 0.5f*(end.y + start.y), this.gameObject.transform.localPosition.z);
+
 		float angle = 0;
 		if (dir.x >= 0) {
 			if (dir.y < 0) {
@@ -45,7 +53,7 @@ public class mapArrow : MonoBehaviour {
 
 	public void setLine(GameObject s, GameObject e){
 		start = s.transform.localPosition;
-		this.gameObject.transform.localPosition = new Vector3 (start.x, start.y, this.gameObject.transform.localPosition.z);
+		//this.gameObject.transform.localPosition = new Vector3 (start.x, start.y, this.gameObject.transform.localPosition.z);
 		end = e.transform.localPosition;
 		OnModify ();
 	}
@@ -66,39 +74,42 @@ public class mapArrow : MonoBehaviour {
 //	}
 
 	public void makeLine(float length){
-		if (createdObject == null) {
-			createdObject = new List<GameObject> ();
-		}
-
-		Image image = mapArrowUnit.GetComponent<Image> ();
-		float curlength = image.rectTransform.rect.width;
-		int i = 0;
-		for (; curlength <= length; ++i) {
-			Debug.Log (createdObject.Count + "," + i );
-			GameObject unit = null;
-			if (createdObject.Count <= i) {
-				unit = (GameObject)GameObject.Instantiate (mapArrowUnit);
-				createdObject.Add (unit);
-			} else {
-				unit = createdObject [i];
-			}
-			if(!unit.activeSelf){
-				unit.SetActive (true);
-			}
-			unit.transform.SetParent (this.gameObject.transform);
-			unit.transform.localPosition = new Vector3 (image.rectTransform.rect.width*i, 0, 0);
-			unit.transform.localRotation = Quaternion.Euler(0, 0, 0);
-			unit.transform.localScale = Vector3.one;
-			curlength += image.rectTransform.rect.width;
-		}
-
-		for(;i < createdObject.Count; ++i){
-			createdObject[i].SetActive(false);
-		}
+		Debug.LogError (sRenderer.sprite.textureRect.width + ","+ sRenderer.sprite.textureRect.height);
+		Vector2 unit = new Vector2( sRenderer.sprite.textureRect.width, sRenderer.sprite.textureRect.height);
+		int num = Mathf.CeilToInt(length / sRenderer.sprite.textureRect.width);
+		sRenderer.size = new Vector2 (sRenderer.sprite.textureRect.width / this.gameObject.transform.localScale.x * num, sRenderer.sprite.textureRect.height / this.gameObject.transform.localScale.y);
+//		if (createdObject == null) {
+//			createdObject = new List<GameObject> ();
+//		}
+//
+//		Image image = mapArrowUnit.GetComponent<Image> ();
+//		float curlength = image.rectTransform.rect.width;
+//		int i = 0;
+//		for (; curlength <= length; ++i) {
+//			Debug.Log (createdObject.Count + "," + i );
+//			GameObject unit = null;
+//			if (createdObject.Count <= i) {
+//				unit = (GameObject)GameObject.Instantiate (mapArrowUnit);
+//				createdObject.Add (unit);
+//			} else {
+//				unit = createdObject [i];
+//			}
+//			if(!unit.activeSelf){
+//				unit.SetActive (true);
+//			}
+//			unit.transform.SetParent (this.gameObject.transform);
+//			unit.transform.localPosition = new Vector3 (image.rectTransform.rect.width*i, 0, 0);
+//			unit.transform.localRotation = Quaternion.Euler(0, 0, 0);
+//			unit.transform.localScale = Vector3.one;
+//			curlength += image.rectTransform.rect.width;
+//		}
+//
+//		for(;i < createdObject.Count; ++i){
+//			createdObject[i].SetActive(false);
+//		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//makeLine (length);
 	}
 }
