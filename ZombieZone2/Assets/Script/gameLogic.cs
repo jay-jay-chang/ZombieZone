f4ac6df3bc77c5a15c4f4c9deae45937ca34bd75
jay <jay@maicity.com>
2017-09-01 19:39:25 +0800

    power gen

---
+++ ZombieZone2/Assets/Script/gameLogic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameLogic : MonoBehaviour {
+
+	public Text foodText;
+	public Text waterText;
+	public Text powerText;
+
+	public sceneController sc;
+
+	public float powerProduceInterval = 1.0f;
+	public int PowerProduce = 15;
+	public float PowerDecayRate = 0.01f;
+	private float powerProduceTime = 0f;
+	private int PowerHealthMax;
+	private int powerHealth;
+	public int PowerHealth{
+		set{ powerHealth = value >= 0 ? value : 0;}
+		get{ return powerHealth; }
+	}
+
+	public float powerComsumeInterval = 1.0f;
+	public int PowerComsume = 10;
+	private float powerComsumeTime = 0f;
+
+	private int food;
+	private int water;
+	private int power;
+
+
+	public int Food{
+		set{ food = value >= 0 ? value : 0; foodText.text = food.ToString(); }
+		get{ return food; }
+	}
+		
+	public int Water{
+		set{ water = value >= 0 ? value : 0; waterText.text = water.ToString();}
+		get{ return water; }
+	}
+
+	public int Power{
+		set{ power = value >= 0 ? value : 0; powerText.text = power.ToString();}
+		get{ return power; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		PowerHealthMax = PowerHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		powerProduceTime += Time.deltaTime;
+		if(powerProduceTime >= powerProduceInterval){
+			powerProduceTime -= powerProduceInterval;
+			Power += Mathf.CeilToInt(((float)PowerHealth)/PowerHealthMax*PowerProduce);
+			PowerHealth -= Mathf.CeilToInt( PowerDecayRate * PowerHealthMax );
+		}
+
+		powerComsumeTime += Time.deltaTime;
+		if (powerComsumeTime >= powerComsumeInterval) {
+			powerComsumeTime -= powerComsumeInterval;
+			if (Power >= PowerComsume) {
+				Power -= PowerComsume;
+				sc.IsLight = true;
+			} else {
+				sc.IsLight = false;
+			}
+		}
+	}
+}
--- ZombieZone2/Assets/Script/sceneController.cs
+++ ZombieZone2/Assets/Script/sceneController.cs
@@ -11,11 +11,48 @@ public class sceneController : MonoBehaviour {
 	public Image[] 	lights;
 	public Image	compuerLight;
 	public Image	warningLen;
-	public bool 	isLight = false;
+	private bool	isLight = false;
+	public bool 	IsLight{
+		get{ return isLight; }
+		set{ 
+			if(isLight != value){
+				isLight = value;
+				if (isLight) {
+					foreach (Image img in darkItems) {
+						img.color = lightcolor;
+					}
+
+					foreach (Image img in lights) {
+						img.enabled = true;
+					}
+
+					compuerLight.enabled = true;
+
+					warningLen.color = new Color(warningLenColer.r*lightcolor.r,  
+						warningLenColer.g*lightcolor.g, 
+						warningLenColer.b*lightcolor.b, 
+						warningLenColer.a*lightcolor.a);
+				} else {
+					foreach (Image img in darkItems) {
+						img.color = darkcolor;
+					}
+
+					foreach (Image img in lights) {
+						img.enabled = false;
+					}
+
+					compuerLight.enabled = false;
+
+					warningLen.color = new Color(warningLenColer.r*darkcolor.r,  
+						warningLenColer.g*darkcolor.g, 
+						warningLenColer.b*darkcolor.b, 
+						warningLenColer.a*darkcolor.a);
+				}
+			}
+		}
+	}
 
 	Color warningLenColer;
-
-	float time = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,42 +64,5 @@ public class sceneController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
-		if (time > 5) {
-			isLight = !isLight;
-			time = 0;
-		}
-
-		if (isLight) {
-			foreach (Image img in darkItems) {
-				img.color = lightcolor;
-			}
-
-			foreach (Image img in lights) {
-				img.enabled = true;
-			}
-
-			compuerLight.enabled = true;
-
-			warningLen.color = new Color(warningLenColer.r*lightcolor.r,  
-											warningLenColer.g*lightcolor.g, 
-											warningLenColer.b*lightcolor.b, 
-											warningLenColer.a*lightcolor.a);
-		} else {
-			foreach (Image img in darkItems) {
-				img.color = darkcolor;
-			}
-
-			foreach (Image img in lights) {
-				img.enabled = false;
-			}
-
-			compuerLight.enabled = false;
-
-			warningLen.color = new Color(warningLenColer.r*darkcolor.r,  
-				warningLenColer.g*darkcolor.g, 
-				warningLenColer.b*darkcolor.b, 
-				warningLenColer.a*darkcolor.a);
-		}
 	}
 }
